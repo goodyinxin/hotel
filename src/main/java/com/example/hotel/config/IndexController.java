f4e4abea84e41af111faf34926b001fe965c9abe
yinxin <179323568@qq.com>
2020-01-06 00:15:11 +0800

    第一次提交
    Signed-off-by: yinxin <179323568@qq.com>

---
+++ src/main/java/com/example/hotel/config/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.hotel.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+        @RequestMapping("/")
+        public String index()  {
+            return "forward:index.html";
+        }
+    }
+
