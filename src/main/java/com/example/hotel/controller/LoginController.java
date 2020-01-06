66ea0045ca4029207043db8c60decaa35fe94a5d
yinxin <179323568@qq.com>
2020-01-07 00:22:03 +0800

    第一次提交
    Signed-off-by: yinxin <179323568@qq.com>

--- src/main/java/com/example/hotel/controller/LoginController.java
+++ src/main/java/com/example/hotel/controller/LoginController.java
@@ -1,13 +1,27 @@
 package com.example.hotel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
 
 
+    @PostMapping("/user/login")
+    public String login(@RequestParam String username, @RequestParam String password){
+        if(!StringUtils.isEmpty(username) &&   "123456".equals(password)){
+            return "dashboard";
+        }else {
+            Map map = new HashMap();
+            map.put("msg","请输入密码");
+            return "index";
+        }
 
-    public void login(){
 
     }
 
